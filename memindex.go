ac71b3290cb4f15d667ba6025f22cdcbacef6b47
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-05-23 08:30:05 +0530

    Add MemIndex - RAM representation of the Index

--- memindex.go
+++ memindex.go
@@ -1,17 +1,54 @@
 package kdb
 
+import (
+	"fmt"
+)
+
+// Struct representing an element in the index. Here we are maintaining a
+// tree structure. So, it's `Values` field only containes in the leaf nodes only
+// `Map` only conatains in root and intermediate nodes only
+//
+// Here all the data elements are on the lowest level, which are leafs
 type IndexElement struct {
 	Values        []string
 	BlockPosition int64
 	Map           map[string]*IndexElement
 }
 
+// Base struct of the MemIndex
+// It contains an element called `root` which is the starting point of the tree
+// orderedKeys contains all the keys in this index in ordered fashion
 type MemIndex struct {
 	root        *IndexElement
 	orderedKeys []string
 }
 
-func (m MemIndex) AddItem(element map[string]string, blockPosition int64) {
+// Create a new MemIndex for a given set of keys
+// All the items we add needs to have all the keys defined here
+// Here's an example:
+
+// ```
+// index := NewMemIndex(string[]{"appId", "metric", "host"})
+// var blockPosition int64 = 1000;
+// item := map[string]string{"appId": "kadira", "metric": "cpu", "host": "h1"}
+// err := index.addItem(item, blockPosition)
+// fmt.Println(err)
+
+// err, element := index.getElement(item)
+// fmt.Println("blockPosition is:", element.BlockPosition)
+// ```
+
+func NewMemIndex(orderedKeys []string) MemIndex {
+	index := MemIndex{}
+	index.root = makeIndexElement()
+	index.orderedKeys = orderedKeys
+
+	return index
+}
+
+// Add Item to the index with the blockPosition
+// return an error, if the item does not have all the keys needs by the index
+func (m MemIndex) AddItem(item map[string]string, blockPosition int64) error {
 	immediateRoot := m.root
 	lenKeys := len(m.orderedKeys)
 
@@ -19,9 +56,13 @@ func (m MemIndex) AddItem(element map[string]string, blockPosition int64) {
 	el.Values = make([]string, lenKeys)
 	el.BlockPosition = blockPosition
 
-	for lc := 0; lc < lenKeys-1; lc++ {
-		key := m.orderedKeys[lc]
-		value := element[key]
+	for lc, key := range m.orderedKeys[0 : lenKeys-1] {
+		value := item[key]
+
+		if value == "" {
+			return fmt.Errorf("no value for `%s`", key)
+		}
+
 		newImmediateRoot := immediateRoot.Map[value]
 		if newImmediateRoot == nil {
 			newImmediateRoot = makeIndexElement()
@@ -33,18 +74,90 @@ func (m MemIndex) AddItem(element map[string]string, blockPosition int64) {
 	}
 
 	lastKey := m.orderedKeys[lenKeys-1 : lenKeys][0]
-	lastValue := element[lastKey]
+	lastValue := item[lastKey]
+
+	if lastValue == "" {
+		return fmt.Errorf("no value for `%s`", lastKey)
+	}
+
 	el.Values[lenKeys-1] = lastValue
 
 	immediateRoot.Map[lastValue] = &el
+
+	return nil
 }
 
-func NewMemIndex(orderedKeys []string) MemIndex {
-	index := MemIndex{}
-	index.root = makeIndexElement()
-	index.orderedKeys = orderedKeys
+// Get the IndexElement related to item
+// With that we can get the BlockPosition we've added
+// Return an error if item does not have all the keys in the index
+func (m MemIndex) GetElement(item map[string]string) (error, *IndexElement) {
+	values := make([]string, len(m.orderedKeys))
 
-	return index
+	for lc, key := range m.orderedKeys {
+		value := item[key]
+		if value == "" {
+			return fmt.Errorf("no value for '%s'", key), nil
+		}
+
+		values[lc] = value
+	}
+
+	el := m.root
+	for _, value := range values {
+		el = el.Map[value]
+		if el == nil {
+			return nil, nil
+		}
+	}
+
+	return nil, el
+}
+
+// HighLevel function to find elements in the index
+// If we've an index with keys `appId`, `metric`, `host`, we can query for
+// subset of index.
+// for an example, we can query for
+//	* all the items of a given appId
+//	* all the cpu meterics of a given appId
+//	* all the cpu meterics of a given appId and host
+//
+// ```
+// _, elements := index.FindElement(map[string]string{"appId": "abc", "metric": "cpu"})
+// ```
+func (m MemIndex) FindElements(query map[string]string) (error, []*IndexElement) {
+	values := make([]string, 0)
+	elements := make([]*IndexElement, 0)
+
+	for _, key := range m.orderedKeys {
+		value := query[key]
+		if value == "" {
+			break
+		}
+
+		values = append(values, value)
+	}
+
+	el := m.root
+	for _, value := range values {
+		el = el.Map[value]
+		if el == nil {
+			return nil, elements
+		}
+	}
+
+	return nil, findElements(el, elements)
+}
+
+func findElements(el *IndexElement, elements []*IndexElement) []*IndexElement {
+	isRootNode := el.Map != nil
+	if isRootNode {
+		for _, value := range el.Map {
+			elements = findElements(value, elements)
+		}
+		return elements
+	} else {
+		return append(elements, el)
+	}
 }
 
 func makeIndexElement() *IndexElement {
--- memindex_test.go
+++ memindex_test.go
@@ -2,8 +2,11 @@ package kdb
 
 import (
 	"reflect"
+	"sort"
 	"testing"
 )
+
+// Create Index
 
 func TestCreateIndex(t *testing.T) {
 	orderedKeys := []string{"appId", "host"}
@@ -17,12 +20,14 @@ func TestCreateIndex(t *testing.T) {
 	}
 }
 
+// Add to Index
+
 func TestAddItemToIndex(t *testing.T) {
 	index := NewMemIndex([]string{"appId", "host"})
 
-	el := map[string]string{"appId": "kadira", "host": "h1"}
+	item := map[string]string{"appId": "kadira", "host": "h1"}
 	var blockPosition int64 = 10
-	index.AddItem(el, blockPosition)
+	index.AddItem(item, blockPosition)
 
 	addedItem := index.root.Map["kadira"].Map["h1"]
 
@@ -39,16 +44,166 @@ func TestAddItemToIndex(t *testing.T) {
 func TestAddItemToIndexOverride(t *testing.T) {
 	index := NewMemIndex([]string{"appId", "host"})
 
-	el := map[string]string{"appId": "kadira", "host": "h1"}
+	item := map[string]string{"appId": "kadira", "host": "h1"}
 	var blockPosition int64 = 10
-	index.AddItem(el, blockPosition)
+	index.AddItem(item, blockPosition)
 
 	blockPosition = 100
-	index.AddItem(el, blockPosition)
+	index.AddItem(item, blockPosition)
 
 	addedItem := index.root.Map["kadira"].Map["h1"]
 
 	if addedItem.BlockPosition != blockPosition {
 		t.Error("block position does not get overridden")
+	}
+}
+
+func TestAddItemToIndexWithInsuffiecientKeys(t *testing.T) {
+	index := NewMemIndex([]string{"appId", "host"})
+
+	item := map[string]string{"appId": "kadira"}
+	var blockPosition int64 = 10
+	err := index.AddItem(item, blockPosition)
+
+	if err == nil {
+		t.Error("need to have an error", err)
+	}
+}
+
+func TestAddItemToIndexWithNoKeys(t *testing.T) {
+	index := NewMemIndex([]string{"appId", "host"})
+
+	item := map[string]string{}
+	var blockPosition int64 = 10
+	err := index.AddItem(item, blockPosition)
+
+	if err == nil {
+		t.Error("need to have an error", err)
+	}
+}
+
+// GetElement
+
+func TestGetElement(t *testing.T) {
+	index := NewMemIndex([]string{"appId", "host"})
+
+	item := map[string]string{"appId": "kadira", "host": "h1"}
+	var blockPosition int64 = 10
+	index.AddItem(item, blockPosition)
+
+	_, addedItem := index.GetElement(item)
+
+	if addedItem == nil {
+		t.Error("couldn't get the added item")
+	}
+
+	if addedItem.BlockPosition != blockPosition {
+		t.Error("block position must be present")
+	}
+
+	expectedValues := []string{"kadira", "h1"}
+	if !reflect.DeepEqual(addedItem.Values, expectedValues) {
+		t.Error("values are incorrect")
+	}
+}
+
+func TestGetElementNoSuchElement(t *testing.T) {
+	index := NewMemIndex([]string{"appId", "host"})
+
+	item := map[string]string{"appId": "kadira", "host": "h1"}
+	var blockPosition int64 = 10
+	index.AddItem(item, blockPosition)
+
+	nonExistingEl := map[string]string{"appId": "kadira", "host": "h2"}
+	err, addedItem := index.GetElement(nonExistingEl)
+
+	if err != nil {
+		t.Error("cannot have a error")
+	}
+
+	if addedItem != nil {
+		t.Error("cannot have element")
+	}
+}
+
+func TestGetElementInsufficientKeys(t *testing.T) {
+	index := NewMemIndex([]string{"appId", "host"})
+
+	item := map[string]string{"appId": "kadira", "host": "h1"}
+	var blockPosition int64 = 10
+	index.AddItem(item, blockPosition)
+
+	query := map[string]string{"appId": "kadira"}
+	err, _ := index.GetElement(query)
+
+	if err == nil {
+		t.Error("need to have an error")
+	}
+}
+
+// Find Elements
+
+func TestFindElementsInBottom(t *testing.T) {
+	index := NewMemIndex([]string{"appId", "host"})
+
+	item := map[string]string{"appId": "kadira", "host": "h1"}
+	var blockPosition int64 = 10
+	index.AddItem(item, blockPosition)
+
+	_, elements := index.FindElements(item)
+
+	if len(elements) != 1 {
+		t.Error("could not find the element")
+	}
+
+	if elements[0].BlockPosition != blockPosition {
+		t.Error("found the wrong element")
+	}
+}
+
+func TestFindElementsInMiddleRoot(t *testing.T) {
+	index := NewMemIndex([]string{"appId", "host"})
+	index.AddItem(map[string]string{"appId": "kadira", "host": "h1"}, 10)
+	index.AddItem(map[string]string{"appId": "kadira", "host": "h2"}, 20)
+	index.AddItem(map[string]string{"appId": "some-other", "host": "h2"}, 20)
+
+	_, elements := index.FindElements(map[string]string{"appId": "kadira"})
+
+	if len(elements) != 2 {
+		t.Error("could not find the element")
+	}
+
+	blockPositions := make([]int64, len(elements))
+	for lc, el := range elements {
+		blockPositions[lc] = el.BlockPosition
+	}
+
+	if !reflect.DeepEqual(blockPositions, []int64{10, 20}) {
+		t.Error("found wrong elements")
+	}
+}
+
+func TestFindElementsInTopLevel(t *testing.T) {
+	index := NewMemIndex([]string{"appId", "host"})
+	index.AddItem(map[string]string{"appId": "kadira", "host": "h1"}, 10)
+	index.AddItem(map[string]string{"appId": "kadira", "host": "h2"}, 20)
+	index.AddItem(map[string]string{"appId": "some-other", "host": "h2"}, 30)
+	index.AddItem(map[string]string{"appId": "coolio", "host": "h2"}, 40)
+
+	_, elements := index.FindElements(map[string]string{})
+
+	if len(elements) != 4 {
+		t.Error("could not find the element")
+	}
+
+	blockPositions := make([]int, len(elements))
+	for lc, el := range elements {
+		blockPositions[lc] = int(el.BlockPosition)
+	}
+
+	sort.Ints(blockPositions)
+
+	if !reflect.DeepEqual(blockPositions, []int{10, 20, 30, 40}) {
+		t.Error("found wrong elements", blockPositions)
 	}
 }
