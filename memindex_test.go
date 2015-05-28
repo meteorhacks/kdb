55a9f0f067b69f64f169850e6ae8fbeab06d86c3
Muhammed Thanish <mnmtanish@gmail.com>
2015-05-28 09:33:06 +0530

    Add missing tests for memindex

--- memindex_test.go
+++ memindex_test.go
@@ -81,7 +81,28 @@ func TestNewMemIndexExistingFile(t *testing.T) {
 }
 
 func TestNewMemIndexCorruptFile(t *testing.T) {
-	// TODO
+	fpath := "/tmp/i1"
+	defer os.Remove(fpath)
+
+	fd, err := os.OpenFile(fpath, os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fd.WriteString("blah blah blah")
+
+	if err := fd.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = NewMemIndex(MemIndexOpts{
+		FilePath:   fpath,
+		IndexDepth: 4,
+	})
+
+	if err == nil {
+		t.Fatal("should return an error")
+	}
 }
 
 func TestMemIndexAdd(t *testing.T) {
@@ -112,14 +133,74 @@ func TestMemIndexAdd(t *testing.T) {
 }
 
 func TestMemIndexGet(t *testing.T) {
-	// TODO
+	fpath := "/tmp/i1"
+	defer os.Remove(fpath)
+
+	idx, err := NewMemIndex(MemIndexOpts{
+		FilePath:   fpath,
+		IndexDepth: 4,
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	vals := []string{"a", "b", "c", "d"}
+	_, err = idx.Add(vals, 100)
+
+	el2, err := idx.Get(vals)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if el2.Position != 100 {
+		t.Fatal("should return correct position")
+	}
 }
 
 func TestMemIndexFind(t *testing.T) {
-	// TODO
+	fpath := "/tmp/i1"
+	defer os.Remove(fpath)
+
+	idx, err := NewMemIndex(MemIndexOpts{
+		FilePath:   fpath,
+		IndexDepth: 4,
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = idx.Add([]string{"a", "b", "c", "d"}, 100)
+	_, err = idx.Add([]string{"a", "b", "c", "e"}, 200)
+	_, err = idx.Add([]string{"a", "b", "f", "d"}, 300)
+
+	els, err := idx.Find([]string{"a", "b", "", "d"})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(els) != 2 {
+		t.Fatal("should return correct number of elements")
+	}
+
+	var el1, el2 *IndexElement
+	if els[0].Position == 100 {
+		el1 = els[0]
+		el2 = els[1]
+	} else {
+		el1 = els[1]
+		el2 = els[0]
+	}
+
+	if el1.Position != 100 || el2.Position != 300 {
+		t.Fatal("should return correct elements")
+	}
 }
 
 func BenchmarkMemIndexAdd(b *testing.B) {
+	b.StopTimer()
+
 	fpath := "/tmp/i1"
 	defer os.Remove(fpath)
 
@@ -141,6 +222,95 @@ func BenchmarkMemIndexAdd(b *testing.B) {
 
 		b.StartTimer()
 		_, err := idx.Add(vals, 100)
+		b.StopTimer()
+
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkMemIndexGet(b *testing.B) {
+	b.StopTimer()
+
+	fpath := "/tmp/i1"
+	defer os.Remove(fpath)
+
+	idx, err := NewMemIndex(MemIndexOpts{
+		FilePath:   fpath,
+		IndexDepth: 4,
+	})
+
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	vals := []string{"a", "b", "c", "d"}
+	_, err = idx.Add(vals, 100)
+
+	for i := 0; i < b.N; i++ {
+		b.StartTimer()
+		_, err := idx.Get(vals)
+		b.StopTimer()
+
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkMemIndexFindQueryInMiddle(b *testing.B) {
+	b.StopTimer()
+
+	fpath := "/tmp/i1"
+	defer os.Remove(fpath)
+
+	idx, err := NewMemIndex(MemIndexOpts{
+		FilePath:   fpath,
+		IndexDepth: 4,
+	})
+
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	_, err = idx.Add([]string{"a", "b", "c", "d"}, 100)
+	_, err = idx.Add([]string{"a", "b", "c", "e"}, 200)
+	_, err = idx.Add([]string{"a", "b", "f", "d"}, 300)
+
+	for i := 0; i < b.N; i++ {
+		b.StartTimer()
+		_, err = idx.Find([]string{"a", "b", "", "d"})
+		b.StopTimer()
+
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkMemIndexFindQueryAtEnd(b *testing.B) {
+	b.StopTimer()
+
+	fpath := "/tmp/i1"
+	defer os.Remove(fpath)
+
+	idx, err := NewMemIndex(MemIndexOpts{
+		FilePath:   fpath,
+		IndexDepth: 4,
+	})
+
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	_, err = idx.Add([]string{"a", "b", "c", "d"}, 100)
+	_, err = idx.Add([]string{"a", "b", "c", "e"}, 200)
+	_, err = idx.Add([]string{"a", "b", "f", "d"}, 300)
+
+	for i := 0; i < b.N; i++ {
+		b.StartTimer()
+		_, err = idx.Find([]string{"a", "b", "c", ""})
 		b.StopTimer()
 
 		if err != nil {
